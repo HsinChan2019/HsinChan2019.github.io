5ae52c50ac04980aafb06ad240f94b7c13a748e3
十里 <55118239+HsinChan2019@users.noreply.github.com>
2023-08-25 15:43:32 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 window.onload = function() {
-  document.getElementById('main-content').innerHTML = 'HsinChan 春风十里'
+  document.getElementById('main-content').innerHTML = '十里  HsinChan'
+  document.getElementById('tab0-content').innerHTML = 'CSDN'
+  document.getElementById('tab1-content').innerHTML = 'QQ群'
+  document.getElementById('tab2-content').innerHTML = '店铺'
 }
