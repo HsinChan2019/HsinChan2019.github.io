a769c7cabb7b0bd3ecb7d4440425c387f25195a2
十里 <55118239+HsinChan2019@users.noreply.github.com>
2023-08-25 19:07:03 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 window.onload = function() {
   document.getElementById('main-content').innerHTML = '十里  HsinChan'
   document.getElementById('info-content').innerHTML = '个人资料简介...'
-  document.getElementById('CSDN-content').innerHTML = 'CSDN：https://blog.csdn.net/qq_45522493?type=blog'
+  
+  var csdn = document.getElementById('CSDN-content');
+  csdn.href = "https://blog.csdn.net/qq_45522493?type=blog";
+  csdn.innerText = "CSDN";
+  
   document.getElementById('QQ群-content').innerHTML = 'QQ群：807206291'
   document.getElementById('店铺-content').innerHTML = '店铺：暂无'
 }
